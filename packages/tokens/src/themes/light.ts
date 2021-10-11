f819331b77908226502c636d2e7a29f6de93b3ac
Joel Henrique Silva Santos <joelhenrique2000@gmail.com>
2021-10-11 00:19:50 -0300

    :sparkles: #24 Adicionado a cor brown

--- packages/tokens/src/themes/light.ts
+++ packages/tokens/src/themes/light.ts
@@ -215,16 +215,16 @@ const deepOranges = {
 }
 
 const browns = {
-    brown50: '#',
-    brown100: '#',
-    brown200: '#',
-    brown300: '#',
-    brown400: '#',
-    brown500: '#',
-    brown600: '#',
-    brown700: '#',
-    brown800: '#',
-    brown900: '#',
+    brown50: '#EFEBE9',
+    brown100: '#D7CCC8',
+    brown200: '#BCAAA4',
+    brown300: '#A1887F',
+    brown400: '#8D6E63',
+    brown500: '#795548',
+    brown600: '#6D4C41',
+    brown700: '#5D4037',
+    brown800: '#4E342E',
+    brown900: '#3E2723',
 }
 
 const grays = {
