aa4ab6e4bb55d138576c23d36b85c589d41bb69a
Joel Henrique Silva Santos <joelhenrique2000@gmail.com>
2021-10-11 00:23:29 -0300

    :sparkles: #24 Adicionado a cor yellow

--- packages/tokens/src/themes/light.ts
+++ packages/tokens/src/themes/light.ts
@@ -163,16 +163,16 @@ const limes = {
 }
 
 const yellows = {
-    yellow50: '#',
-    yellow100: '#',
-    yellow200: '#',
-    yellow300: '#',
-    yellow400: '#',
-    yellow500: '#',
-    yellow600: '#',
-    yellow700: '#',
-    yellow800: '#',
-    yellow900: '#',
+    yellow50: '#FFFDE7',
+    yellow100: '#FFF9C4',
+    yellow200: '#FFF59D',
+    yellow300: '#FFF176',
+    yellow400: '#FFEE58',
+    yellow500: '#FFEB3B',
+    yellow600: '#FDD835',
+    yellow700: '#FBC02D',
+    yellow800: '#F9A825',
+    yellow900: '#F57F17',
 }
 
 const ambers = {
