c220a1d9e378dece1ea802a8310d452d4a27947d
Joel Henrique Silva Santos <joelhenrique2000@gmail.com>
2021-10-11 00:28:16 -0300

    :sparkles: #24 Adicionado a cor blue

--- packages/tokens/src/themes/light.ts
+++ packages/tokens/src/themes/light.ts
@@ -72,16 +72,16 @@ const indigos = {
 }
 
 const blues = {
-    blue50: '#',
-    blue100: '#',
-    blue200: '#',
-    blue300: '#',
-    blue400: '#',
-    blue500: '#',
-    blue600: '#',
-    blue700: '#',
-    blue800: '#',
-    blue900: '#',
+    blue50: '#E3F2FD',
+    blue100: '#BBDEFB',
+    blue200: '#90CAF9',
+    blue300: '#64B5F6',
+    blue400: '#42A5F5',
+    blue500: '#2196F3',
+    blue600: '#1E88E5',
+    blue700: '#1976D2',
+    blue800: '#1565C0',
+    blue900: '#0D47A1',
 }
 
 const lightBlues = {
