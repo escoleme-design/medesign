03faadc5d8c2b2657bcd1c40d62028fcc9edc550
Joel Henrique Silva Santos <joelhenrique2000@gmail.com>
2021-10-11 00:18:09 -0300

    :sparkles: #24 Adicionado a cor gray

--- packages/tokens/src/themes/light.ts
+++ packages/tokens/src/themes/light.ts
@@ -228,16 +228,16 @@ const browns = {
 }
 
 const grays = {
-    gray50: '#',
-    gray100: '#',
-    gray200: '#',
-    gray300: '#',
-    gray400: '#',
-    gray500: '#',
-    gray600: '#',
-    gray700: '#',
-    gray800: '#',
-    gray900: '#',
+    gray50: '#FAFAFA',
+    gray100: '#F5F5F5',
+    gray200: '#EEEEEE',
+    gray300: '#E0E0E0',
+    gray400: '#BDBDBD',
+    gray500: '#9E9E9E',
+    gray600: '#757575',
+    gray700: '#616161',
+    gray800: '#424242',
+    gray900: '#212121',
 }
 
 const blueGrays = {
