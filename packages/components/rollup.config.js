afc72e05de937af99baddecdca2667067d794ef6
Joel Henrique Silva Santos <joelhenrique2000@gmail.com>
2021-10-12 23:04:08 -0300

    Atualizado o rollup do components package

--- packages/components/rollup.config.js
+++ packages/components/rollup.config.js
@@ -1,41 +1,14 @@
-import babel from 'rollup-plugin-babel'
-import commonjs from '@rollup/plugin-commonjs'
-import resolve from '@rollup/plugin-node-resolve'
-import {terser} from 'rollup-plugin-terser'
-import {visualizer} from 'rollup-plugin-visualizer'
-import peerDepsExternal from 'rollup-plugin-peer-deps-external';
-import alias from '@rollup/plugin-alias';
-import path from "path";
+import babel from "rollup-plugin-babel";
 
-const extensions = ['.js', '.jsx', '.ts', '.tsx']
-
-const formats = ['esm', 'umd']
-
-const plugins = [
-  babel({extensions, exclude: 'node_modules/**', runtimeHelpers: true}),
-  resolve({extensions}),
-  commonjs(),
-  alias({}),
-  terser(),
-  peerDepsExternal(),
-  visualizer({sourcemap: true}),
-]
+const plugins = [babel({ exclude: "node_modules/**" })];
 
 export default [
   {
-    input: 'src/index.jsx',
-    external: ['styled-components', 'react', 'react-dom'],
+    input: "src/index.js",
     plugins,
-    output: formats.map(format => ({
-      file: `dist/component.${format}.js`,
-      format,
-      sourcemap: true,
-      name: '@escoleme/medesign-components',
-      globals: {
-        react: 'React',
-        'react-dom': 'ReactDOM',
-        'styled-components': 'styled'
-      }
-    }))
-  }
-]
+    output: {
+      file: `dist/index.js`,
+      format: 'esm',
+    },
+  },
+];
---
+++ packages/docs/src/index.stories.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import { Button } from '@escoleme/mecomponents';
+
+export default { title: 'Button' };
+
+export const withText = () => <Button>Hello Button</Button>;
