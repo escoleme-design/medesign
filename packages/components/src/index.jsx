cc71103de28686052e488128b46c89ba72726568
Joel Henrique Silva Santos <joelhenrique2000@gmail.com>
2021-10-12 23:07:33 -0300

    Simplificado o components

--- packages/components/src/index.jsx
+++ packages/components/src/index.jsx
@@ -1,16 +1,7 @@
 "use strict";
 
 import React from 'react';
-import { light } from "@escoleme/medesign-tokens";
 
 export function Button(props) {
-  const { children } = props;
-
-  const buttonStyle = {
-    color: light.colors.white,
-    backgroundColor: light.colors.primary,
-    padding: light.spacings.s14
-  };
-
-  return <button style={buttonStyle}>{children}</button>;
+  return <button>{children}</button>;
 }
