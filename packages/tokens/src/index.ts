1990a4b719cf7ed1505b49e512e9766b6ca116c5
Joel Henrique Silva Santos <joelhenrique2000@gmail.com>
2021-11-02 16:14:44 -0300

    deletado as partes que serão refatoradas

--- packages/tokens/src/index.ts
+++ packages/tokens/src/index.ts
@@ -1,9 +1,9 @@
 import * as Types from './types';
 import * as lightBase from './themes/light';
 
-export { themePropType } from './utils/theme-prop-type';
-
-const light: Types.Theme = { ...lightBase };
+const light: Types.Theme = {
+    ...lightBase
+};
 
 export type Theme = Types.Theme;
 export { light };
--- packages/tokens/src/utils/colors.ts
+++
@@ -1,5 +0,0 @@
-const chroma = require('chroma-js')
-
-export const darken = (color: string, saturacao: number, light: number): string => {
-    return chroma.hsl(chroma(color).hsl()[0], saturacao, light).hex()
-}
